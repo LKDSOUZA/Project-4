c0d224df32499aab6ffbfedfd09c0f7130c1dfea
Zakia Aziz <akhi5000@yahoo.com>
2023-03-18 09:51:25 +1100

    first commit

---
+++ app.py
@@ -0,0 +1,96 @@
+
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///offstreetcarpark.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+Parking = Base.classes.parking
+Business = Base.classes.business
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/Parking<br/>"
+        f"/api/v1.0/Business"
+    )
+
+
+@app.route("/api/v1.0/Parking")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    
+    # Query all parking data
+    results = session.query(Parking.census_year,Parking.block_id,Parking.parking_type,Parking.parking_spaces,Parking.latitude,Parking.latitude).all()
+
+    session.close()
+
+    
+    # Create a dictionary from the row data and append to a list of all_parkings
+    all_parkings = []
+    for census_year,block_id,parking_type,parking_spaces,longitude,latitude in results:
+        parking_dict = {}
+        parking_dict["census_year"] = census_year
+        parking_dict["block_id"] = block_id
+        parking_dict["parking_type"] = parking_type
+        parking_dict["parking_spaces"] = parking_spaces
+        parking_dict["longitude"] = longitude
+        parking_dict["latitude"] = latitude
+        all_parkings.append(parking_dict)
+
+    return jsonify(all_parkings)
+
+@app.route("/api/v1.0/Business")
+def business():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    
+    # Query all business data
+    results = session.query(Business.census_year,Business.block_id,Business.longitude, Business.latitude).all()
+
+    session.close()
+
+    
+    # Create a dictionary from the row data and append to a list of all_parkings
+    all_business = []
+    for census_year,block_id,longitude,latitude in results:
+        business_dict = {}
+        business_dict["census_year"] = census_year
+        business_dict["block_id"] = block_id
+        business_dict["longitude"] = longitude
+        business_dict["latitude"] = latitude
+        all_business.append(business_dict)
+
+    return jsonify(all_business)
+if __name__ == '__main__':
+    app.run(debug=True)
+
